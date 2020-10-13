c5aab1d8252c2d25131e9f88f433638928dc60c1
gitynity <nitybriliantluckybro@gmail.com>
2020-10-13 14:29:23 +0530

    Update levelorder traversal.cpp

--- levelorder traversal.cpp
+++ levelorder traversal.cpp
@@ -16,10 +16,10 @@ struct node
 };
 
 
-vector<int> v;
-
-vector<int> levelordertraversal(node*root)
+vector<int> levelordertraversal1(node*root)     //left2right-top2bottom
 {
+    vector<int> v;
+    
 	if(!root) return v;
 	
 	queue<node*> q;
@@ -43,6 +43,96 @@ vector<int> levelordertraversal(node*root)
 	return v;
 }
 
+vector<int> levelordertraversal2(node*root)     //right2left-top2bottom
+{
+    vector<int> v;
+    
+	if(!root) return v;
+	
+	queue<node*> q;
+	
+	q.push(root);
+	
+	while(!q.empty())
+	{
+		auto temp = q.front();
+		
+		if(temp->right)
+			q.push(temp->right);
+		
+		if(temp->left)
+			q.push(temp->left);
+		
+		v.push_back(temp->value);
+		
+		q.pop();
+	}
+	return v;
+}
+
+vector<int> levelordertraversal3(node*root)     //left2right-bottom2top
+{
+    vector<int> v;
+    
+	if(!root) return v;
+	
+	queue<node*> q;
+	
+	q.push(root);
+	
+	while(!q.empty())
+	{
+		auto temp = q.front();
+		
+		if(temp->right)
+			q.push(temp->right);
+		
+		if(temp->left)
+			q.push(temp->left);
+		
+		v.push_back(temp->value);
+		
+		q.pop();
+	}
+	reverse(v.begin(),v.end());
+	return v;
+}
+
+void print_vector(vector<int>& v)
+{
+    for(auto i:v)
+		cout<<i<<" ";
+	cout<<"\n";
+	v.clear();
+}
+
+vector<int> levelordertraversal4(node*root)     //right2left-bottom2top
+{
+    vector<int> v;
+    
+	if(!root) return v;
+	
+	queue<node*> q;
+	
+	q.push(root);
+	
+	while(!q.empty())
+	{
+		auto temp = q.front();
+		
+		if(temp->left)
+			q.push(temp->left);
+		
+		if(temp->right)
+			q.push(temp->right);
+		
+		v.push_back(temp->value);
+		
+		q.pop();
+	}
+	reverse(v.begin(),v.end());
+	return v;
+}
 
 int main()
 {
@@ -55,16 +145,40 @@ int main()
 	n1->left = n2;
 	n1->right = n3;
     
-  node* n4 = new node(4,0,0);
+    node* n4 = new node(4,0,0);
     
-  n2->left = n4;
+    n2->left = n4;
     
-  //node n5(5,0,0); will be stored in stack memory
+    /*
+    
+            1
+           / \
+          2   3
+         /
+        4  
+    
+    */
+    
+    //node n5(5,0,0); will be stored in stack memory
+    //node* n6 = new node(6,0,0); will be stored in heap memory
     
 	std::vector<int> res;
-	res = levelordertraversal(n1);
-	for(auto i:res)
-		cout<<i<<" ";
+	res = levelordertraversal1(n1);
+	cout<<"Top to Bottom and Left to Light\n";
+	print_vector(res);
+	
+	res = levelordertraversal2(n1);
+	cout<<"Top to Bottom and Right to Left\n";
+	print_vector(res);
+	
+	res = levelordertraversal3(n1);
+	cout<<"Bottom to Top and Left to Right\n";
+	print_vector(res);
+
+    res = levelordertraversal4(n1);
+    cout<<"Bottom to Top and Right to Left\n";
+    print_vector(res);
+	
 	return 0;
 	
 	
