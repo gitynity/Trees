8c5ae7a3f5700a2a639f3249c38514b27efd0f11
gitynity <nitybriliantluckybro@gmail.com>
2020-10-13 13:37:11 +0530

    Update DFS traversal.cpp

--- DFS traversal.cpp
+++ DFS traversal.cpp
@@ -1,50 +1,61 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 struct node
 {
 	int val;
 	node* left;
 	node* right;
+	
+	node(int x , node*l , node*r)
+	{
+	    this->val = x;
+	    this->left = l;
+	    this->right = r;
+	}
 };
 
-void preorder(node*root , vector<int>& v;)
+void preorder(node*root , vector<int>& v)
 {
 	if(!root)
 		return;
 	v.push_back(root->val);
-	preorder(root->left);
-	preorder(root->right);
+	preorder(root->left,v);
+	preorder(root->right,v);
 }
 
-void inorder(node*root , vector<int>& v;)
+void inorder(node*root , vector<int>& v)
 {
 	if(!root)
 		return;
-	inorder(root->left);
+	inorder(root->left,v);
 	v.push_back(root->val);
-	inorder(root->right);
+	inorder(root->right,v);
 }
 
-void postorder(node*root , vector<int>& v;)
+void postorder(node*root , vector<int>& v)
 {
 	if(!root)
 		return;
-	postorder(root->left);
-	postorder(root->right);
+	postorder(root->left,v);
+	postorder(root->right,v);
 	v.push_back(root->val);
 	
 }
 
 void print_vector(vector<int> v)
 {
-for(auto i:res)
+for(auto i:v)
   cout<<i<<" ";
 cout<<endl;
 }
 
 int main()
 {
-node n1 = new node();
-node n2 = new node();
-node n3 = new node();
+node* n1 = new node(1,0,0);
+node* n2 = new node(2,0,0);
+node* n3 = new node(3,0,0);
+
 n1->left = n2;
 n1->right = n3;
 
